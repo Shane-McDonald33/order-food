2c66f04f75cde7c9c3feae6738ad0311abfb8589
Shane-McDonald33 <mcdonaldshane33@gmail.com>
2022-02-24 22:56:45 -0600

    finished, can order items to the backend and fully functioning cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
@@ -8,6 +8,8 @@ import Checkout from './Checkout'
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -23,7 +25,21 @@ const Cart = (props) => {
 
   const orderHandler = () => {
     setIsCheckout(true)//all this does is swtiching isCheckout to true
-  }
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch('https://react-posting-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    });
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -40,23 +56,33 @@ const Cart = (props) => {
     </ul>
   );
 
-  const modalActions = <div className={classes.actions}>
+  const modalActions = (<div className={classes.actions}>
   <button className={classes['button--alt']} onClick={props.onClose}>
     Close
   </button>
   {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}{/*The function this button relays to turns isCheckout state to true, triggering the Checkout component to show up*/}
   </div>
+  );
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
+  const cartModalContent = (<React.Fragment>
+    {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose}/>} {/*This tell us that this component is conditional on certain state being true, NOTE: we initially set isCheckout to false in state, but here, isCheckout is truthy because it is in the curlies, this whole segment is depending on the orderHandler function*/}
+      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>} {/*This tell us that this component is conditional on certain state being true, NOTE: we initially set isCheckout to false in state, but here, isCheckout is truthy because it is in the curlies, this whole segment is depending on the orderHandler function*/}
       {/*For line above, onCancel is props handled in Checkout component, looks like onclick-props.onCancel, so onCancel is the props call here*/}
       {!isCheckout && modalActions}{/*Gets rid of the 'order' and a'clsoe' buttons when isCheckout is false, which in this case is after we click on order...I'm still a little confused on this one*/}
+  </React.Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending Order Data...</p>;
+  const didSubmitModalContent = <p>Successfully Sent The Order!</p>
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
